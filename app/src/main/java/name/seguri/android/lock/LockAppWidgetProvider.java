e27571dd370425d5a8f2aafc4ae8271e1078eb7f
chenhl@koal.com <chenhl@koal.com>
2024-11-06 17:42:26 +0800

    widget

--- app/src/main/java/name/seguri/android/lock/LockAppWidgetProvider.java
+++ app/src/main/java/name/seguri/android/lock/LockAppWidgetProvider.java
@@ -6,6 +6,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 public class LockAppWidgetProvider extends AppWidgetProvider {
 
